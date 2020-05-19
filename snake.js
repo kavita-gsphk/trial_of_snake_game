4056ea55664af38987dbf935aeccef1cbeacfb5c
kavita-gsphk <63964682+kavita-gsphk@users.noreply.github.com>
2020-05-19 18:37:11 +0530

    Add files via upload

--- snake.js
+++ snake.js
@@ -2,6 +2,9 @@ if (window.innerHeight > 510 && window.innerWidth > 510) {
   const cvs = document.getElementById("snake");
   const ctx = cvs.getContext("2d");
   const fruit = cvs.getContext("2d");
+  let count;
+  let popup_1;
+  count = 0;
   const box = 30;
   cvs.width = 510;
   cvs.height = 510;
@@ -101,6 +104,19 @@ if (window.innerHeight > 510 && window.innerWidth > 510) {
     }
     return false;
   }
+  let popup = () => {
+    console.log(1);
+    document.querySelector(".popup").classList.toggle("popup-hidden");
+    document.querySelector("#context").innerHTML = "Your Score: " + score;
+    clearInterval(popup_1);
+  };
+  let popup_2 = () => {
+    console.log(1);
+    document.querySelector(".popup").classList.toggle("popup-hidden");
+    location.reload();
+  };
+  document.querySelector("button").addEventListener("click", popup_2);
+  document.querySelector("#cancel").addEventListener("click", popup);
 
   function draw() {
     ctx.drawImage(ground, 0, 0);
@@ -178,6 +194,7 @@ if (window.innerHeight > 510 && window.innerWidth > 510) {
       clearInterval(game_5);
       clearInterval(game_6);
       clearInterval(game_7);
+      count++;
     }
     if (snakeX == 5 * box && snakeY > 11 * box) {
       clearInterval(game);
@@ -188,6 +205,7 @@ if (window.innerHeight > 510 && window.innerWidth > 510) {
       clearInterval(game_5);
       clearInterval(game_6);
       clearInterval(game_7);
+      count++;
     }
     if (snakeX > 6 * box && snakeY == 8 * box) {
       clearInterval(game);
@@ -198,6 +216,7 @@ if (window.innerHeight > 510 && window.innerWidth > 510) {
       clearInterval(game_5);
       clearInterval(game_6);
       clearInterval(game_7);
+      count++;
     }
     if (snakeX < 3 * box && snakeY == 5 * box) {
       clearInterval(game);
@@ -208,7 +227,9 @@ if (window.innerHeight > 510 && window.innerWidth > 510) {
       clearInterval(game_5);
       clearInterval(game_6);
       clearInterval(game_7);
+      count++;
     }
+
     snake.unshift(newHead);
     ctx.fillStyle = "black";
     ctx.font = "40px Changa one";
@@ -220,7 +241,12 @@ if (window.innerHeight > 510 && window.innerWidth > 510) {
     ctx.fillText(speed, 7 * box, 1.5 * box);
     ctx.strokeText(rate, 13 * box, 1.5 * box);
     ctx.fillText(rate, 13 * box, 1.5 * box);
+    if (count == 1) {
+      count++;
+      popup_1 = setInterval(popup, 450);
+    }
   }
+
   ctx.canvas.addEventListener("click", decrese);
   function decrese() {
     console.log(rate);
@@ -274,6 +300,7 @@ if (window.innerHeight > 510 && window.innerWidth > 510) {
       game_6 = setInterval(draw, 80);
     }
   }
+
   let game = setInterval(draw, 200);
 }
 
@@ -292,6 +319,9 @@ else {
   cvs.width = window.innerWidth;
   cvs.height = window.innerHeight;
   const max_width = window.innerWidth;
+  let count;
+  count = 0;
+  let popup_1;
   const max_hight = 100 + "%";
   const box_w = Math.floor(window.innerWidth / 20);
   const box_h = Math.floor(window.innerHeight / 30);
@@ -334,9 +364,6 @@ else {
   ctx.canvas.addEventListener("click", decrese);
   document.addEventListener("keydown", direction);
   document.addEventListener("touchstart", s);
-  document.addEventListener("touchmove", m);
-  //document.addEventListener("touchend", e);
-  //document.addEventListener("touchcancel", c);
   function direction(event) {
     let key = event.keyCode;
     if (key == 37 && d != "right") {
@@ -379,33 +406,7 @@ else {
       }
     }
   }
-  /*function c(event) {
-    p_m_x = event.targetTouches[0].clientX;
-    p_m_y = event.targetTouches[0].clientY;
-  }*/
-  //console.log(touches[0].clientX);
-  /* function e(event) {
-    if (p_m_x - p_s_x >= 60 && d != "left") {
-      if (p_m_y - p_s_y < 60 || p_s_y - p_m_y < 60) {
-        d = "right";
-      }
-    }
-    if (p_s_x - p_m_x >= 60 && d != "right") {
-      if (p_m_y - p_s_y < 60 || p_s_y - p_m_y < 60) {
-        d = "left";
-      }
-    }
-    if (p_m_y - p_s_y >= 60 && d != "up") {
-      if (p_m_x - p_s_x < 60 || p_s_x - p_m_x < 60) {
-        d = "down";
-      }
-    }
-    if (p_s_y - p_m_y >= 60 && d != "down") {
-      if (p_m_x - p_s_x < 60 || p_s_x - p_m_x < 60) {
-        d = "up";
-      }
-    }
-  }*/
+
   function collision(head, array) {
     for (let i = 0; i < array.length; i++) {
       if (head.x == array[i].x && head.y == array[i].y) {
@@ -414,6 +415,19 @@ else {
     }
     return false;
   }
+  let popup = () => {
+    console.log(1);
+    document.querySelector(".popup").classList.toggle("popup-hidden");
+    document.querySelector("#context").innerHTML = "Your Score: " + score;
+    clearInterval(popup_1);
+  };
+  let popup_2 = () => {
+    console.log(1);
+    document.querySelector(".popup").classList.toggle("popup-hidden");
+    location.reload();
+  };
+  document.querySelector("button").addEventListener("click", popup_2);
+  document.querySelector("#cancel").addEventListener("click", popup);
   function draw() {
     ctx.drawImage(ground, 0, 0);
     ctx.drawImage(nag, 13 * box_w, 0.75 * box_h);
@@ -524,6 +538,7 @@ else {
       clearInterval(game_5);
       clearInterval(game_6);
       clearInterval(game_7);
+      count++;
     }
     if (snakeX < 7 * box_w && snakeY == 26 * box_h) {
       clearInterval(game);
@@ -534,6 +549,7 @@ else {
       clearInterval(game_5);
       clearInterval(game_6);
       clearInterval(game_7);
+      count++;
     }
     if (snakeX > 7 * box_w && snakeX < 16 * box_w && snakeY == 18 * box_h) {
       clearInterval(game);
@@ -544,6 +560,7 @@ else {
       clearInterval(game_5);
       clearInterval(game_6);
       clearInterval(game_7);
+      count++;
     }
     if (snakeX > 7 * box_w && snakeX < 13 * box_w && snakeY == 22 * box_h) {
       clearInterval(game);
@@ -554,6 +571,7 @@ else {
       clearInterval(game_5);
       clearInterval(game_6);
       clearInterval(game_7);
+      count++;
     }
     if (snakeX > 7 * box_w && snakeX < 16 * box_w && snakeY == 8 * box_h) {
       clearInterval(game);
@@ -564,6 +582,7 @@ else {
       clearInterval(game_5);
       clearInterval(game_6);
       clearInterval(game_7);
+      count++;
     }
     if (snakeX == 8 * box_w && snakeY > 7 * box_h && snakeY < 18 * box_h) {
       clearInterval(game);
@@ -574,6 +593,7 @@ else {
       clearInterval(game_5);
       clearInterval(game_6);
       clearInterval(game_7);
+      count++;
     }
     if (snakeX > 12 * box_w && snakeX < 20 * box_w && snakeY == 13 * box_h) {
       clearInterval(game);
@@ -584,6 +604,9 @@ else {
       clearInterval(game_5);
       clearInterval(game_6);
       clearInterval(game_7);
+      count++;
+    }
+    if (count == 1) {
     }
     ctx.fillRect(7 * box_w, 0, 3, 2 * box_h);
     snake.unshift(newHead);
@@ -597,6 +620,10 @@ else {
     ctx.fillText(speed, 8 * box_w, 1.5 * box_h);
     ctx.strokeText(rate, 15 * box_w, 1.5 * box_h);
     ctx.fillText(rate, 15 * box_w, 1.5 * box_h);
+    if (count == 1) {
+      count++;
+      popup_1 = setInterval(popup, 450);
+    }
   }
   ctx.canvas.addEventListener("click", decrese);
   function decrese() {
